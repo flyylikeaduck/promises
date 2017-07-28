ce6351141c88b22db32563d0903d34863e3efc5b
Hack Reactor Student <student@hackreactor.com>
2017-07-27 21:30:47 -0700

    passed pluckfirstline

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,28 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  var firstLine = '';
+  fs.readFile(filePath, (err, data) => {
+    if (!data) {  
+      callback(err, data);
+    } else {
+      var stringed = data.toString();
+      firstLine += stringed.slice(0, stringed.indexOf('\n'));
+      callback(err, firstLine);
+    }
+  });
+  
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  stringed = '';
+  request.on('data', chunk => {
+    stringed += chunk.toString();
+    console.log(stringed);
+  })
+  callback(err, statusCode);
 };
 
 // Export these functions so we can test them and reuse them in later exercises
